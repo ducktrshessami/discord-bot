f91cbc267f1b39019b70460aa90ffda46b6def00
ducktrshessami <jwee761@gmail.com>
2020-11-08 13:22:59 -0800

    restructure js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,10 @@
 const discord = require("discord.js"); // Discord
+const Command = require("./command"); // Bot command
+const Response = require("./response"); // Bot response
 
 const defaultServerOptions = { // Default server options for new servers
     name: "",
     prefix: undefined
-};
-const defaultCMDOptions = { // Default command options
-    usage: "",
-    description: "",
-    subtitle: "",
-    hidden: false,
-    admin: false
-};
-const defaultResponseOptions = { // Default response options
-    userWhitelist: [],
-    userBlacklist: [],
-    serverWhitelist: [],
-    serverBlacklist: [],
-    channelWhitelist: [],
-    channelBlacklist: []
 };
 
 class DiscordBot extends discord.Client { // Main bot class
@@ -119,107 +106,6 @@ class DiscordBot extends discord.Client { // Main bot class
     }
 };
 
-class Command { // Bot command class
-    constructor(cmd, cb, options = {}) {
-        this.name = cmd.toLowerCase().trim();
-        this.callback = cb;
-        this.usage = options.usage || defaultCMDOptions.usage;
-        this.description = options.description || defaultCMDOptions.description;
-        this.subtitle = options.subtitle || defaultCMDOptions.subtitle;
-        this.hidden = options.hidden || defaultCMDOptions.hidden;
-        this.admin = options.admin || defaultCMDOptions.admin;
-    }
-    
-    check(message, prefix = "", admin = false, execute = true) { // Check if a message matches this command
-        var args = getArgs(message, prefix);
-        if (args[0].toLowerCase() == this.name && (!this.admin || admin)) {
-            if (execute) {
-                this.exec(message, args);
-            }
-            return true;
-        }
-        return false;
-    }
-    
-    exec(message, args) { // Execute this command
-        this.callback(message, args);
-    }
-};
-
-class Response { // Bot response class
-    constructor(tr, rs, cf, rf = defaultResponseFunction, options = {}) {
-        this.trigger = tr;
-        this.response = rs;
-        this.responseFunction = rf;
-        this.userWhitelist = options.userWhitelist || defaultResponseOptions.userWhitelist;
-        this.userBlacklist = options.userBlacklist || defaultResponseOptions.userBlacklist;
-        this.serverWhitelist = options.serverWhitelist || defaultResponseOptions.serverWhitelist;
-        this.serverBlacklist = options.serverBlacklist || defaultResponseOptions.serverBlacklist;
-        this.channelWhitelist = options.channelWhitelist || defaultResponseOptions.channelWhitelist;
-        this.channelBlacklist = options.channelBlacklist || defaultResponseOptions.channelBlacklist;
-        if (typeof this.trigger == "string") {
-            this.checkFunction = cf || defaultSimpleCheckFunction; // Response based on whole message
-        }
-        else {
-            this.checkFunction = cf || defaultCheckFunction; // Response based on keywords
-        }
-    }
-    
-    check(message, execute = true) { // Check if a message would trigger this response
-        if (this._listCheck(message) && this.checkFunction(message, this.trigger)) {
-            if (execute) {
-                this.say(message);
-            }
-            return true;
-        }
-        return false;
-    }
-    
-    say(message) { // Respond to a message
-        this.responseFunction(message, this.response);
-    }
-    
-    _listCheck(message) { // Check if the right conditions are met
-        var user = true, server = true, channel = true;
-        if (this.userWhitelist.length) { // Check user lists
-            user = this.userWhitelist.includes(message.author.id);
-        }
-        else if (this.userBlacklist.length) {
-            user = !this.userBlacklist.includes(message.author.id);
-        }
-        if (this.serverWhitelist.length) { // Check server lists
-            server = this.serverWhitelist.includes(message.guild.id);
-        }
-        else if (this.serverBlacklist.length) {
-            server = !this.serverWhitelist.includes(message.guild.id);
-        }
-        if (this.channelWhitelist.length) { // Check channel lists
-            channel = this.channelWhitelist.includes(message.channel.id);
-        }
-        else if (this.channelBlacklist.length) {
-            channel = !this.channelWhitelist.includes(message.channel.id);
-        }
-        return user && server && channel;
-    }
-};
-
-function getArgs(message, prefix) { // Get args for a command from a message
-    return message.content.toLowerCase().substring(prefix.length).replace(/\n/g, ' ').split(' ');
-}
-
-function defaultSimpleCheckFunction(message, trigger) { // Default checking function for string triggers
-    return message.content.toLowerCase().trim() == trigger.toLowerCase().trim();
-}
-
-function defaultCheckFunction(message, trigger) { // Default checking function for keyword-based triggers
-    return trigger.every(tr => message.content.toLowerCase().includes(tr.toLowerCase().trim()));
-}
-
-function defaultResponseFunction(message, response) { // Default function for responding
-    message.channel.send(response).catch(err => message.client.emit("error", err));
-}
-
 module.exports = DiscordBot; // Export things
-module.exports.getArgs = getArgs;
 module.exports.Command = Command;
 module.exports.Response = Response;
