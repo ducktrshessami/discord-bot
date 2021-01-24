0d6299edb9e964506f5e68989aec2f9461a5f299
ducktrshessami <jwee761@gmail.com>
2021-01-24 13:49:43 -0800

    default command options test

--- test/command.test.js
+++ test/command.test.js
@@ -9,6 +9,16 @@ const memberCmd = new Command("bar", works, { aliases: ["b"] });
 
 describe("Command", function() {
     describe("#constructor", function() {
+        it("fills in default values for unset options", function() {
+            let cmd = new Command("foo");
+            assert.strictEqual(typeof cmd.usage, "string");
+            assert.strictEqual(typeof cmd.description, "string");
+            assert.strictEqual(typeof cmd.subtitle, "string");
+            assert.strictEqual(typeof cmd.hidden, "boolean");
+            assert.strictEqual(typeof cmd.owner, "boolean");
+            assert.strictEqual(typeof cmd.admin, "boolean");
+            assert(cmd.aliases instanceof Array);
+        });
         it("auto formats command name", function() {
             const unformatted = " cHiCkEn\n", formatted = "chicken";
             let cmd = new Command(unformatted, works);
