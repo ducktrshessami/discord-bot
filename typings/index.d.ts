409275eeb65ff4962ee5bb87dc11c33059a1df7a
ducktrshessami <jwee761@gmail.com>
2021-04-04 12:07:22 -0700

    declare utils in ts

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -126,6 +126,39 @@ declare module "discord-bot" {
 
             private _listCheck(message: discord.Message): Boolean;
         }
+
+        namespace utils {
+            function awaitResponse(
+                channel: discord.TextChannel,
+                content?: discord.StringResolvable | discord.APIMessage,
+                options?: discord.MessageOptions | discord.MessageAdditions,
+                messageTest: (reply: discord.Message) => Boolean,
+                ms: Number,
+                verbose?: Boolean
+            ): Promise<discord.Message | void>;
+            function logMessage(message: discord.Message): discord.Message;
+            function reactButtons(
+                message: discord.Message,
+                reactHandlers?: Array<{
+                    emoji: discord.EmojiIdentifierResolvable,
+                    callback: (reaction: discord.MessageReaction, user: discord.User) => void;
+                }>,
+                ms: Number,
+                maxMs?: Number
+            ): Promise<discord.Message>;
+            function sendPages(
+                channel: discord.TextChannel,
+                pages: Array<{
+                    content?: discord.StringResolvable | discord.APIMessage,
+                    options?: discord.MessageOptions | discord.MessageAdditions
+                }>,
+                left?: discord.EmojiIdentifierResolvable,
+                right?: discord.EmojiIdentifierResolvable,
+                ms: Number,
+                maxMs?: Number
+            ): Promise<discord.Message>;
+            function sendVerbose(channel: discord.TextChannel, content?: discord.StringResolvable | discord.APIMessage, options?: discord.MessageOptions | discord.MessageAdditions): Promise<discord.Message>;
+        }
     }
 
     export = DiscordBot;
